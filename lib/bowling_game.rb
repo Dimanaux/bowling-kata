388833115d1f12b637513e83969c87e8572dca74
Dmitry Barskov <barskovdmitry42@gmail.com>
2020-04-21 10:32:51 +0300

    update bowling_game implementation

--- lib/bowling_game.rb
+++ lib/bowling_game.rb
@@ -1,9 +1,30 @@
 class BowlingGame
-  def initialize(pins)
-    @pins = pins
+  def rolls(rolls)
+    @rolls = rolls
+    self
   end
 
   def score
-    0
+    total = 0
+    i = 0
+    while i < @rolls.size
+      if @rolls[i] == 10 # strike
+        total += 10
+        if i < @rolls.size - 3 # not last frame
+          total += @rolls[i + 1] + @rolls[i + 2]
+        end
+        i += 1
+      elsif i < @rolls.size - 1 && @rolls[i] + @rolls[i + 1] == 10 # spare
+        total += 10
+        if i < @rolls.size - 3 # not last frame
+          total += @rolls[i + 2]
+        end
+        i += 2
+      else
+        total += @rolls[i] + (@rolls[i + 1] || 0)
+        i += 2
+      end
+    end
+    total
   end
 end
